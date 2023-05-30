30c6859e23f347aaeb2dea8839fdf11f0fd2f7f1
Valdem88 <valdem88@gmail.com>
2023-05-30 23:02:25 +0300

    DZ_dev-17_kuber-homeworks-clopro-15.2

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "yandex_cloud_id" {
+  default = "b1g4m8qmhdgh8k9b16jr"
+}
+
+variable "yandex_folder_id" {
+  default = "b1g72hofn0r6eom3l3oo"
+}
+
+variable "a-zone" {
+  default = "ru-central1-a"
+}
+
+variable "lamp-instance-image-id" {
+  default = "fd827b91d99psvq5fjit"
+}
